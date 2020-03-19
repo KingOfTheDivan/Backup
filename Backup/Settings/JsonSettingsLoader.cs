57a9e79c1e9b69c14a6b70d0b3b6989875e21a18
kotd <>
2020-03-19 20:20:50 +0300

    complete feature: load settings from xml and json files

---
+++ Backup/Settings/JsonSettingsLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace BackupFile.Settings
+{
+    public class JsonSettingsLoader : ISettingsLoader
+    {
+        /// <summary>
+        /// Загружает данные из файла настроек, десериализуя его в экземпляр класса Settings
+        /// </summary>
+        /// <param name="file">Имя файла настроек</param>
+        /// <returns>Экземпляр класса Settings</returns>
+        public Settings Load(string file)
+        {
+            return JsonConvert.DeserializeObject<Settings>(File.ReadAllText(file));
+        }
+        
+    }
+
+}
